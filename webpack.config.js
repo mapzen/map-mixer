350b0289de251466f89172035a4d6658866e5ca3
Hanbyul Jo <hanbyul@nyu.edu>
2017-11-16 16:50:39 -0500

    replaced babel preset es2015 to bable preset env

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
             ['transform-react-jsx']
           ],
           presets: [
-            ['es2015', { modules: false }]
+            ['env']
           ]
         }
       }]
