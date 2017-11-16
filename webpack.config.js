4d29ecf506a791429a8aba38dbeb91f7c94b5bf3
Hanbyul Jo <hanbyul@nyu.edu>
2017-11-16 16:10:12 -0500

    added foundation code to start

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    entry: './index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public/scripts'),
+    publicPath: 'scripts/',
+    filename: 'bundle.js'
+  },
+  module: {
+    // Apply `noParse` to Tangram to prevent mangling of UMD boilerplate
+    noParse: /tangram\/dist\/tangram/,
+    rules: [{
+      test: /\.jsx?$/,
+      include: path.resolve(__dirname, 'src'),
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          plugins: [
+            ['transform-react-jsx']
+          ],
+          presets: [
+            ['es2015', { modules: false }]
+          ]
+        }
+      }]
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      // This is required to bundle images in Leaflet's stylesheet.
+      test: /\.png$/,
+      use: 'url-loader'
+    }]
+  },
+};
